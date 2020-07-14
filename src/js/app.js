51b50af7577ba2222df3c2de188a796fedc0d862
Roland Weinzierl <weinro@gmail.com>
2020-07-14 17:52:52 +0200

    Letzte Korrekturen
    Nach Built von Jannes am 14.07.

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,17 @@ const SCROLL = document.querySelector('#scroll-down');
 const LEGAL = document.querySelector('#legal');
 const LOGO = document.querySelector('#logo');
 
+document.querySelectorAll('.scroll-bottom').forEach(el => {
+    el.addEventListener('click', () => {
+        console.log('scroll-to-bottom');
+        CONTAINERS[CONTAINERS.length - 1].scrollIntoView({
+            behavior: 'smooth',
+        });
+    });
+});
+
+let activeIndex = 0;
+
 WRAPPER.addEventListener('scroll', event => {
     // show legal links if scrolled to bottom
     let c = CONTENT.getBoundingClientRect();
@@ -37,10 +48,22 @@ WRAPPER.addEventListener('scroll', event => {
         -2 +
         25}%, #4e125a ${scrollRelative / -2 + 125}%)`;
 
-    CONTAINERS.forEach(element => {
+    CONTAINERS.forEach((element, i) => {
         let top = element.getBoundingClientRect().top;
         let opacity = 1 - Math.sqrt(top * top) / (window.innerHeight / 2);
 
         element.style.opacity = opacity;
+
+        if (opacity == 1) {
+            activeIndex = i;
+        }
     });
+});
+
+SCROLL.addEventListener('click', () => {
+    if (activeIndex < CONTAINERS.length - 1) {
+        CONTAINERS[activeIndex + 1].scrollIntoView({
+            behavior: 'smooth',
+        });
+    }
 });
